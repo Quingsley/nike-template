7ae4c34ddfc5c4dfe0224dc52f64f03e300e9e59
quingsley <quingsley@hotmail.com>
2023-04-05 11:07:06 +0300

    added on_boarding to pageView

--- lib/on_boarding/on_boarding_screen.dart
+++ lib/on_boarding/on_boarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'on_boarding_1.dart';
 import 'on_boarding_2.dart';
 import 'on_boarding_3.dart';
+import 'on_boarding_4.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -14,9 +15,10 @@ class OnBoardingScreen extends StatelessWidget {
     return PageView(
       controller: controller,
       children: const <Widget>[
-        // OnBoarding1(bgColor: bgColor),
+        OnBoarding1(bgColor: bgColor),
         OnBoarding2(bgColor: bgColor),
         OnBoarding3(bgColor: bgColor),
+        OnBoarding4(bgColor: bgColor)
       ],
     );
   }
